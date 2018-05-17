c77b862c4dad6d66b0ac54fe20d97e21f0fabe8b
priyaVora <prvora89@gmail.com>
2018-05-16 22:30:37 -0600

    Finished!

--- command_factory_AssignmentImplementation/src/main/java/command/solutions/ItemControl.java
+++ command_factory_AssignmentImplementation/src/main/java/command/solutions/ItemControl.java
@@ -6,28 +6,29 @@ public class ItemControl {
 
 	public static final int ROWS = 50;
 	public static final int COLS = 50;
-	
+
 	private Command[][] buttons = new Command[ROWS][COLS];
-	
+
 	private Stack<Command> commands = new Stack<Command>();
-	
+
 	public void program(int r, int c, Command functionality) {
 		buttons[r][c] = functionality;
 	}
-	
+
 	public void pressButton(int r, int c) {
 		Command cmd = buttons[r][c];
-		if(cmd != null) {
+		if (cmd != null) {
 			cmd.execute();
 			commands.push(cmd.clone());
 		}
 	}
-	
-	public void pressUndoButton() { 
-		if(commands.isEmpty()) { 
-			return;
+
+	public Command pressUndoButton() {
+		if (commands.isEmpty()) {
+			return null;
 		}
 		Command cmd = commands.pop();
 		cmd.undo();
+		return cmd;
 	}
 }
