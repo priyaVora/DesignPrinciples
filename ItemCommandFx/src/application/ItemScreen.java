576b400b602f593abf19287acd98c117751c3e9d
priyaVora <prvora89@gmail.com>
2018-05-16 22:30:07 -0600

    Finished!

--- ItemCommandFx/src/application/ItemScreen.java
+++ ItemCommandFx/src/application/ItemScreen.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import command.models.Item;
+import command.solutions.Command;
 import command.solutions.ItemChangeColorsCommandRed;
 import command.solutions.ItemChangedColorsCommandBlack;
 import command.solutions.ItemControl;
@@ -87,6 +88,56 @@ public class ItemScreen implements Initializable {
 		}
 	}
 
+	public void leftButtonMovement(ItemControl rc) {
+		System.out.println("Left Arrow is Clicked!");
+		if (currentRow != 0) {
+			rc.pressButton(2, 0);
+			currentCol = currentCol;
+			currentRow = currentRow - 1;
+		}
+		System.out.println("Current Row: " + currentRow);
+		System.out.println("Current Col: " + currentCol);
+		setGridView();
+	}
+
+	public void rightButtonMovement(ItemControl rc) {
+		System.out.println("Right Arrow is Clicked!");
+		if (currentRow < 14) {
+			rc.pressButton(4, 0);
+			currentCol = currentCol;
+			currentRow = currentRow + 1;
+		}
+		System.out.println("Current Row: " + currentRow);
+		System.out.println("Current Col: " + currentCol);
+		setGridView();
+	}
+
+	public void topButtonMovement(ItemControl rc) {
+		System.out.println("Top Arrow is Clicked!");
+
+		if (currentCol != 0) {
+			rc.pressButton(5, 0);
+			currentCol = currentCol - 1;
+			currentRow = currentRow;
+		}
+		System.out.println("Current Row: " + currentRow);
+		System.out.println("Current Col: " + currentCol);
+		setGridView();
+	}
+
+	public void bottomButtonMovement(ItemControl rc) {
+		System.out.println("Bottom Arrow is Clicked!");
+
+		if (currentCol < 28) {
+			rc.pressButton(3, 0);
+			currentCol = currentCol + 1;
+			currentRow = currentRow;
+		}
+		System.out.println("Current Row: " + currentRow);
+		System.out.println("Current Col: " + currentCol);
+		setGridView();
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// Image imageDecline = new
@@ -103,58 +154,33 @@ public class ItemScreen implements Initializable {
 				new ItemChangeColorsCommandRed(item, item.getColorChange() != null ? item.getColorChange() : "Red"));
 		rc.program(1, 0, new ItemChangedColorsCommandBlack(item,
 				item.getColorChange() != null ? item.getColorChange() : "Black"));
-		rc.program(2, 0, new ItemMoveCommandTop(item, item.getMovement() != null ? item.getMovement() : "Top"));
+		rc.program(5, 0, new ItemMoveCommandTop(item, item.getMovement() != null ? item.getMovement() : "Top"));
 
-		rc.program(3, 0, new ItemMoveCommandRight(item, item.getMovement() != null ? item.getMovement() : "Right"));
+		rc.program(4, 0, new ItemMoveCommandRight(item, item.getMovement() != null ? item.getMovement() : "Right"));
 
-		rc.program(4, 0, new ItemMoveCommandBottom(item, item.getMovement() != null ? item.getMovement() : "Bottom"));
+		rc.program(3, 0, new ItemMoveCommandBottom(item, item.getMovement() != null ? item.getMovement() : "Bottom"));
 
-		rc.program(5, 0, new ItemMoveCommandLeft(item, item.getMovement() != null ? item.getMovement() : "Left"));
+		rc.program(2, 0, new ItemMoveCommandLeft(item, item.getMovement() != null ? item.getMovement() : "Left"));
 
 		leftButton.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Left Arrow is Clicked!");
-				if (currentRow != 0) {
-					rc.pressButton(2, 0);
-					currentCol = currentCol;
-					currentRow = currentRow - 1;
-				}
-				System.out.println("Current Row: " + currentRow);
-				System.out.println("Current Col: " + currentCol);
-				setGridView();
+				leftButtonMovement(rc);
 			}
 		});
 		rightButton.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Right Arrow is Clicked!");
-				if (currentRow < 14) {
-					rc.pressButton(4, 0);
-					currentCol = currentCol;
-					currentRow = currentRow + 1;
-				}
-				System.out.println("Current Row: " + currentRow);
-				System.out.println("Current Col: " + currentCol);
-				setGridView();
+				rightButtonMovement(rc);
 			}
 		});
 		topButton.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Top Arrow is Clicked!");
-
-				if (currentCol != 0) {
-					rc.pressButton(5, 0);
-					currentCol = currentCol - 1;
-					currentRow = currentRow;
-				}
-				System.out.println("Current Row: " + currentRow);
-				System.out.println("Current Col: " + currentCol);
-				setGridView();
+				topButtonMovement(rc);
 			}
 
 		});
@@ -162,33 +188,88 @@ public class ItemScreen implements Initializable {
 
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Bottom Arrow is Clicked!");
-
-				if (currentCol < 28) {
-					rc.pressButton(3, 0);
-					currentCol = currentCol + 1;
-					currentRow = currentRow;
-				}
-				System.out.println("Current Row: " + currentRow);
-				System.out.println("Current Col: " + currentCol);
-				setGridView();
+				bottomButtonMovement(rc);
 			}
 		});
 
 		undoButton.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Undo!");
-				rc.pressUndoButton();
-				
+				Command cmd = rc.pressUndoButton();
+				System.out.println("////////////////////");
+				if (cmd != null) {
+					if (cmd instanceof ItemMoveCommandTop) {
+						System.out.println("Undo COMMAND WAS: Top");
+						if (currentCol < 28) {
+							currentCol = currentCol + 1;
+							currentRow = currentRow;
+						}
+						System.out.println("Current Row: " + currentRow);
+						System.out.println("Current Col: " + currentCol);
+						setGridView();
+					} else if (cmd instanceof ItemMoveCommandRight) {
+						System.out.println("Undo COMMAND WAS: Right");
+						if (currentRow != 0) {
+							currentCol = currentCol;
+							currentRow = currentRow - 1;
+						}
+						System.out.println("Current Row: " + currentRow);
+						System.out.println("Current Col: " + currentCol);
+						setGridView();
+					} else if (cmd instanceof ItemMoveCommandLeft) {
+						System.out.println("Undo COMMAND WAS: Left");
+						if (currentRow < 14) {
+							currentCol = currentCol;
+							currentRow = currentRow + 1;
+						}
+						System.out.println("Current Row: " + currentRow);
+						System.out.println("Current Col: " + currentCol);
+						setGridView();
+					} else if (cmd instanceof ItemMoveCommandBottom) {
+						System.out.println("Undo COMMAND WAS: Bottom");
+
+						if (currentCol != 0) {
+							currentCol = currentCol - 1;
+							currentRow = currentRow;
+						}
+						System.out.println("Current Row: " + currentRow);
+						System.out.println("Current Col: " + currentCol);
+						setGridView();
+					} else if (cmd instanceof ItemChangeColorsCommandRed) {
+						System.out.println("Undo COMMAND WAS: Red");
+						ColorChangeSelection.setValue(ColorChangeSelection.getItems().get(1));
+						String selected = ColorChangeSelection.getItems()
+								.get(ColorChangeSelection.getSelectionModel().getSelectedIndex());
+
+						if (selected.equals("Red")) {
+							System.out.println(selected);
+						
+						} else if (selected.equals("Black")) {
+							System.out.println("Black");
+					
+						}
+						setGridView();
+					} else if (cmd instanceof ItemChangedColorsCommandBlack) {
+						System.out.println("Undo COMMAND WAS: Black");
+						ColorChangeSelection.setValue(ColorChangeSelection.getItems().get(0));
+						String selected = ColorChangeSelection.getItems()
+								.get(ColorChangeSelection.getSelectionModel().getSelectedIndex());
+
+						if (selected.equals("Red")) {
+							System.out.println(selected);
+		
+						} else if (selected.equals("Black")) {
+							System.out.println("Black");
+		
+						}
+						setGridView();
+					}
+				}
+
 			}
 		});
-		
-		
-		
-		
-		
+
 		ColorChangeSelection.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
@@ -198,10 +279,13 @@ public class ItemScreen implements Initializable {
 
 				if (selected.equals("Red")) {
 					System.out.println(selected);
+					rc.pressButton(0, 0);
 				} else if (selected.equals("Black")) {
 					System.out.println("Black");
+					rc.pressButton(1, 0);
 				}
 				setGridView();
+
 			}
 		});
 
